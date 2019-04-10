e1b592604fc5afaaaad5b344ee3383cacb9316da
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-04-10 22:51:05 +0900

    chore(stack): change order

--- packages/stack/bin/ngs.ts
+++ packages/stack/bin/ngs.ts
@@ -7,8 +7,8 @@ import NgsUsEast1Stack from "../lib/ngs-us-east-1-stack";
 /* eslint-disable no-new */
 const app = new cdk.App();
 
-// deploy to us-east-1
-new NgsUsEast1Stack(app, "NgsUsEast1Stack", { env: { region: "us-east-1" } });
-
 // deploy to your default region
 new NgsStack(app, "NgsStack");
+
+// deploy to us-east-1
+new NgsUsEast1Stack(app, "NgsUsEast1Stack", { env: { region: "us-east-1" } });
