9f9c475fe2fcaa9316cbc6dda6bcbf1c8ba3d12e
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-04-27 15:29:24 +0900

    chore(origin-response): install deps only on build script
    [ci skip]

--- packages/lambda-origin-response/bin/build.sh
+++ packages/lambda-origin-response/bin/build.sh
@@ -3,14 +3,16 @@
 
 # cleanup previous caches
 rm -rf ./lib
-rm -rf ./node_modules
+mv ./node_modules ./node_modules_host
 
 # create and build node_modules
 docker build ./ --tag ngs-lambda-layers
-docker run --rm -v "$PWD":/var/task ngs-lambda-layers yarn install
+docker run --rm -v "$PWD":/var/task ngs-lambda-layers yarn install --prod
 # yarn run build
 
-# build a package for Lambda Layers
+# build a package for Lambda
 mkdir ./lib
 mv ./node_modules ./lib/
+mv ./node_modules_host ./node_modules
 
+yarn run build
