5ce56d0455c524b45b50d3c86f59a4353762b49c
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-04-27 12:57:11 +0900

    chore(test): add test for validator

--- packages/lambda-origin-response/__tests__/validator.test.ts
+++ packages/lambda-origin-response/__tests__/validator.test.ts
@@ -138,102 +138,350 @@ describe("validator", () => {
   });
 
   describe("#validate", () => {
-    describe("parameter a", () => {
-      describe("is valid", () => {
-        it("that passed 0, returns true", async () => {
+    describe("parameter a is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 0", async () => {
           expect(await validate({ a: "0" })).toBeTruthy();
         });
       });
 
-      describe("is invalid", () => {
-        it("that passes -1, returns false", async () => {
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ a: null })).toBeFalsy();
+        });
+
+        it("passed -1", async () => {
           expect(await validate({ a: -1 })).toBeFalsy();
         });
 
-        it("that passed 4, returns false", async () => {
+        it("passed 4", async () => {
           expect(await validate({ a: 4 })).toBeFalsy();
         });
 
-        it("that passed str, returns false", async () => {
+        it("passed str", async () => {
           expect(await validate({ a: "str" })).toBeFalsy();
         });
       });
     });
 
-    describe("parameter b", () => {
-      describe("is valid", () => {
-        it("that passed 1290ad, returns true", async () => {
+    describe("parameter b is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 1290ad", async () => {
           expect(await validate({ b: "1290ad" })).toBeTruthy();
         });
 
-        it("that passed 1290abef, returns true", async () => {
+        it("passed 1290abef", async () => {
           expect(await validate({ b: "1290abef" })).toBeTruthy();
         });
       });
 
-      describe("is invalid", () => {
-        it("that passed ccc, returns false", async () => {
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ b: null })).toBeFalsy();
+        });
+
+        it("passed ccc", async () => {
           expect(await validate({ b: "ccc" })).toBeFalsy();
         });
 
-        it("that passed 1290fg, returns false", async () => {
+        it("passed 1290fg", async () => {
           expect(await validate({ b: "1290fg" })).toBeFalsy();
         });
 
-        it("that passed 1290ff1h, returns false", async () => {
+        it("passed 1290ff1h", async () => {
           expect(await validate({ b: "1290ff1h" })).toBeFalsy();
         });
       });
     });
 
-    describe("parameter c", () => {
-      describe("is valid", () => {
-        test("that passed 1:2:3:4, returns true", async () => {
+    describe("parameter c is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 1:2:3:4", async () => {
           expect(await validate({ c: "1:2:3:4" })).toBeTruthy();
         });
       });
 
-      describe("is invalid", () => {
-        test("that passed 01:2:3:4, returns false", async () => {
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ c: null })).toBeFalsy();
+        });
+
+        it("passed 01:2:3:4", async () => {
           expect(await validate({ c: "01:2:3:4" })).toBeFalsy();
         });
 
-        test("that passed 1:2:3, returns false", async () => {
+        it("passed 1:2:3", async () => {
           expect(await validate({ c: "1:2:3" })).toBeFalsy();
         });
 
-        test("that passed 1:2:3:4:5, returns false", async () => {
+        it("passed 1:2:3:4:5", async () => {
           expect(await validate({ c: "1:2:3:4:5" })).toBeFalsy();
         });
       });
     });
 
-    describe("parameter cr", () => {
-      describe("is valid", () => {
-        test("that passed 0:1:0:1, returns true", async () => {
+    describe("parameter cr is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 0:1:0:1", async () => {
           expect(await validate({ cr: "0:1:0:1" })).toBeTruthy();
         });
 
-        test("that passed 0:0.2:0.123456789:1, returns true", async () => {
+        it("passed 0:0.2:0.123456789:1", async () => {
           expect(await validate({ cr: "0:0.2:0.123456789:1" })).toBeTruthy();
         });
       });
 
-      describe("is invalid", () => {
-        test("that passed 01:0:1:0, returns false", async () => {
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ cr: null })).toBeFalsy();
+        });
+
+        it("passed 01:0:1:0", async () => {
           expect(await validate({ cr: "01:0:1:0" })).toBeFalsy();
         });
 
-        test("that passed 1:0:1, returns false", async () => {
+        it("passed 1:0:1", async () => {
           expect(await validate({ cr: "1:0:1" })).toBeFalsy();
         });
 
-        test("that passed 1:0:1:0:1, returns false", async () => {
+        it("passed 1:0:1:0:1", async () => {
           expect(await validate({ cr: "1:0:1:0:1" })).toBeFalsy();
         });
 
-        test("that passed 1.2:0:1:0, returns false", async () => {
+        it("passed 1.2:0:1:0", async () => {
           expect(await validate({ cr: "1.2:0:1:0" })).toBeFalsy();
+        });
+      });
+    });
+
+    describe("parameter f is", () => {
+      describe("valid (returns true)", () => {
+        it("passed jpg", async () => {
+          expect(await validate({ f: "jpg" })).toBeTruthy();
+        });
+
+        it("passed webp", async () => {
+          expect(await validate({ f: "webp" })).toBeTruthy();
+        });
+      });
+
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ f: null })).toBeFalsy();
+        });
+
+        it("passed bmp", async () => {
+          expect(await validate({ f: "bmp" })).toBeFalsy();
+        });
+      });
+    });
+
+    describe("parameter g is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 1", async () => {
+          expect(await validate({ g: "1" })).toBeTruthy();
+        });
+
+        it("passed 9", async () => {
+          expect(await validate({ g: "9" })).toBeTruthy();
+        });
+      });
+
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ g: null })).toBeFalsy();
+        });
+
+        it("passed 0", async () => {
+          expect(await validate({ g: "0" })).toBeFalsy();
+        });
+
+        it("passed 10", async () => {
+          expect(await validate({ g: "10" })).toBeFalsy();
+        });
+      });
+    });
+
+    describe("parameter h is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 1", async () => {
+          expect(await validate({ h: "1" })).toBeTruthy();
+        });
+
+        it("passed 1080", async () => {
+          expect(await validate({ h: "9999" })).toBeTruthy();
+        });
+      });
+
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ h: null })).toBeFalsy();
+        });
+
+        it("passed 0", async () => {
+          expect(await validate({ h: "0" })).toBeFalsy();
+        });
+
+        it("passed -1", async () => {
+          expect(await validate({ h: "-1" })).toBeFalsy();
+        });
+      });
+    });
+
+    describe("parameter o is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 0", async () => {
+          expect(await validate({ o: "0" })).toBeTruthy();
+        });
+
+        it("passed 1", async () => {
+          expect(await validate({ o: "1" })).toBeTruthy();
+        });
+        it("passed false", async () => {
+          expect(await validate({ o: "false" })).toBeTruthy();
+        });
+
+        it("passed true", async () => {
+          expect(await validate({ o: "true" })).toBeTruthy();
+        });
+      });
+
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ o: null })).toBeFalsy();
+        });
+
+        it("passed str", async () => {
+          expect(await validate({ o: "str" })).toBeFalsy();
+        });
+      });
+    });
+
+    describe("parameter r is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 1", async () => {
+          expect(await validate({ r: "1" })).toBeTruthy();
+        });
+
+        it("passed 8", async () => {
+          expect(await validate({ r: "8" })).toBeTruthy();
+        });
+
+        it("passed auto", async () => {
+          expect(await validate({ r: "auto" })).toBeTruthy();
+        });
+      });
+
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ r: null })).toBeFalsy();
+        });
+
+        it("passed 0", async () => {
+          expect(await validate({ r: "0" })).toBeFalsy();
+        });
+
+        it("passed 9", async () => {
+          expect(await validate({ r: "9" })).toBeFalsy();
+        });
+      });
+    });
+
+    describe("parameter through is", () => {
+      describe("valid (returns true)", () => {
+        it("passed jpg", async () => {
+          expect(await validate({ through: "jpg" })).toBeTruthy();
+        });
+
+        it("passed jpg:png", async () => {
+          expect(await validate({ through: "jpg:png" })).toBeTruthy();
+        });
+      });
+
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ through: null })).toBeFalsy();
+        });
+
+        it("passed bmp", async () => {
+          expect(await validate({ through: "bmp" })).toBeFalsy();
+        });
+
+        it("passed jpg:", async () => {
+          expect(await validate({ through: "jpg:" })).toBeFalsy();
+        });
+
+        it("passed jpg:bmp", async () => {
+          expect(await validate({ through: "jpg:bmp" })).toBeFalsy();
+        });
+      });
+    });
+
+    describe("parameter u is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 0", async () => {
+          expect(await validate({ u: "0" })).toBeTruthy();
+        });
+
+        it("passed 1", async () => {
+          expect(await validate({ u: "1" })).toBeTruthy();
+        });
+        it("passed false", async () => {
+          expect(await validate({ u: "false" })).toBeTruthy();
+        });
+
+        it("passed true", async () => {
+          expect(await validate({ u: "true" })).toBeTruthy();
+        });
+      });
+
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ u: null })).toBeFalsy();
+        });
+
+        it("passed str", async () => {
+          expect(await validate({ u: "str" })).toBeFalsy();
+        });
+      });
+    });
+
+    describe("parameter v is", () => {
+      describe("valid (returns true)", () => {
+        it("passed str", async () => {
+          expect(await validate({ v: "str" })).toBeTruthy();
+        });
+      });
+
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ v: null })).toBeFalsy();
+        });
+      });
+    });
+
+    describe("parameter w is", () => {
+      describe("valid (returns true)", () => {
+        it("passed 1", async () => {
+          expect(await validate({ w: "1" })).toBeTruthy();
+        });
+
+        it("passed 1080", async () => {
+          expect(await validate({ w: "9999" })).toBeTruthy();
+        });
+      });
+
+      describe("invalid (returns false)", () => {
+        it("passed null", async () => {
+          expect(await validate({ h: null })).toBeFalsy();
+        });
+
+        it("passed 0", async () => {
+          expect(await validate({ w: "0" })).toBeFalsy();
+        });
+
+        it("passed -1", async () => {
+          expect(await validate({ w: "-1" })).toBeFalsy();
         });
       });
     });
